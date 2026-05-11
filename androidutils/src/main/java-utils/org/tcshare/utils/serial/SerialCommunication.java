5d66dc5bdab401b7bd23d0b42d11103d7dfe5e05
qiangubafang <ou.leiqi@163.com>
2026-05-11 11:11:04 +0800

    串口调用提供便利方法。

--- androidutils/src/main/java-utils/org/tcshare/utils/serial/SerialCommunication.java
+++ androidutils/src/main/java-utils/org/tcshare/utils/serial/SerialCommunication.java
@@ -2,31 +2,32 @@ package org.tcshare.utils.serial;
 
 import org.tcshare.utils.packet.IPacket;
 import org.tcshare.utils.packet.IPacketCallback;
-import org.tcshare.utils.packet.PacketSimpleUtil;
 
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.nio.ByteBuffer;
 import java.util.concurrent.LinkedBlockingQueue;
 
 /**
  * 异步全双工串口通信功能 <br/>
- * 抽取到代码库 <br/>
- * 数据包格式 <br/>
- *      包头	  长度	  内容	     校验	包尾 <br/>
- *      1	  2	      1-N个字节	 2	    1 <br/>
  */
 public class SerialCommunication {
     private static final String TAG = SerialCommunication.class.getSimpleName();
     private final String port;
     private final int band;
     private final IPacketCallback cb;
-    private final IPacket psu;
+    private IPacket psu;
     private SerialPort mSerialPort;
     private ReadThread mReadThread;
     private WriteThread mWriteThread;
     private boolean connected = false; // 是否链接上了
 
 
+    public SerialCommunication(String port, int band, IPacketCallback cb) {
+        this.port = port;
+        this.band = band;
+        this.cb = cb;
+    }
     public SerialCommunication(String port, int band, IPacket psu, IPacketCallback cb) {
         this.port = port;
         this.band = band;
@@ -35,13 +36,14 @@ public class SerialCommunication {
     }
 
     /**
-     * 仅传递负载，psu 构造包
+     * 仅传递负载
+     * 或使用 psu 构造包
      * @param payload
      * @return
      */
     public boolean sendPacket(byte[] payload) {
         if (mWriteThread != null && connected) {
-            mWriteThread.sendData(psu.gen(payload));
+            mWriteThread.sendData(psu != null ? psu.gen(payload) : payload);
             return true;
         }else{
             return false;
@@ -123,7 +125,12 @@ public class SerialCommunication {
                     if (mInputStream == null) return;
                     size = mInputStream.read(buffer);
                     if (size > 0) {
-                        psu.preparePacket(PacketSimpleUtil.subBytes(buffer, 0, size), cb);
+                        ByteBuffer bb = ByteBuffer.wrap(buffer, 0, size);
+                        if(psu != null) {
+                            psu.preparePacket(bb.array(), cb);
+                        }else{
+                            cb.onPacketReady(bb.array());
+                        }
                     }
                 } catch (Exception e) {
                     e.printStackTrace();
